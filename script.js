344d19ac9f89b29d0942328c54725fe33b3dc0e6
Sudarsan Kumar <sudarsanmitc@gmail.com>
2021-03-05 11:25:16 +0530

    Added source Files

---
+++ script.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+    parent : document.querySelector('.app'),
+    intensity : 0.9,
+    image1 : './img1.jpg',
+    image2 : './img2.jpg',
+    angle : Math.PI/8,
+    displacementImage : './transition.png'
+})
